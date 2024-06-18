40515e991731aaffd680da7ca87c5698ea33968a
KDW <msd030428@gmail.com>
2024-06-19 00:04:30 +0900

    Update Pub&Sub

--- include/mono_visual_odometry/mono_visual_odometry.hpp
+++ include/mono_visual_odometry/mono_visual_odometry.hpp
@@ -1,13 +1,15 @@
 #ifndef __MONO_VISUAL_ODOMETRY_H__
 #define __MONO_VISUAL_ODOMETRY_H__
-
+#include <cv_bridge/cv_bridge.h>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
 #include <opencv2/opencv.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 struct FrameData {
   cv::Mat image;
   std::vector<cv::Point2f> keyPoints;
-  int32_t timestamp;
+  int32_t frameId;
 };
 class MonoVisualOdometry : public rclcpp::Node {
 public:
@@ -18,10 +20,17 @@ private:
   std::vector<cv::Point2f> featureExtract(const cv::Mat &image);
   void featureMatching(const FrameData &prevFrameData, const FrameData &frameData);
   void bundleAdjustment();
+  void debugImagePublish();
+  void pathPublish();
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr imageSub_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr debugImagePub_;
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr pathPub_;
+
+  std::vector<FrameData> frames_;
+  std::vector<geometry_msgs::msg::PoseStamped> poses_;
 };
 
 #endif // __MONO_VISUAL_ODOMETRY_H__
--- src/mono_visual_odometry.cpp
+++ src/mono_visual_odometry.cpp
@@ -2,6 +2,9 @@
 MonoVisualOdometry::MonoVisualOdometry(const rclcpp::NodeOptions &options) : Node("mono_visual_odometry", options) {
   RCLCPP_INFO(this->get_logger(), "MonoVisualOdometry");
 
+  debugImagePub_ = this->create_publisher<sensor_msgs::msg::Image>("/stereo/image", 10);
+  pathPub_ = this->create_publisher<nav_msgs::msg::Path>("/stereo/path", 10);
+
   auto interval = std::chrono::duration<double>(1.0);
   timer_ = this->create_wall_timer(interval, [this]() -> void { RCLCPP_INFO(this->get_logger(), "Timer RUN!!"); });
 }
@@ -18,3 +21,20 @@ std::vector<cv::Point2f> MonoVisualOdometry::featureExtract(const cv::Mat &image
 }
 
 void MonoVisualOdometry::featureMatching(const FrameData &prevFrameData, const FrameData &frameData) {}
+
+void MonoVisualOdometry::debugImagePublish() {
+  if (frames_.size() > 0) {
+    auto message = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frames_.end()->image).toImageMsg();
+    message->header.stamp = this->get_clock()->now();
+    debugImagePub_->publish(*message);
+  }
+}
+
+void MonoVisualOdometry::pathPublish() {
+  nav_msgs::msg::Path pathMsg;
+  pathMsg.header.stamp = this->now();
+  pathMsg.header.frame_id = "map";
+  pathMsg.poses = poses_;
+
+  pathPub_->publish(pathMsg);
+}
