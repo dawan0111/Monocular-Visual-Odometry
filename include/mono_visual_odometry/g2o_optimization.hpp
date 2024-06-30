b6cc211307adf5a2d56856ace4100f7728e8def6
KDW <msd030428@gmail.com>
2024-06-30 18:58:08 +0900

    Update G2O Optimization impl

--- include/mono_visual_odometry/g2o_optimization.hpp
+++ include/mono_visual_odometry/g2o_optimization.hpp
@@ -1,30 +1,36 @@
 #ifndef __G2O_OPTIMIZATION_H__
 #define __G2O_OPTIMIZATION_H__
-
+#include <Eigen/Dense>
 #include <g2o/core/base_binary_edge.h>
 #include <g2o/core/base_vertex.h>
 #include <g2o/core/block_solver.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
 #include <g2o/core/robust_kernel_impl.h>
 #include <g2o/solvers/csparse/linear_solver_csparse.h>
+#include <g2o/types/sba/types_six_dof_expmap.h>
+#include <sophus/se3.hpp>
 
 class G2O_Optimization {
   using BlockSolverType = g2o::BlockSolver<g2o::BlockSolverTraits<6, 3>>;
   using LinearSolverType = g2o::LinearSolverCSparse<BlockSolverType::PoseMatrixType>;
-  using WorldPoints = std::vector<Eigen::Vector3d>;
-  using CameraPoints = std::vector<Eigen::Vector2d>;
 
 public:
-  G2O_Optimization();
+  G2O_Optimization(Eigen::Matrix3d camK);
   ~G2O_Optimization();
-  void updatePoseVertex();
-  void updateEdge();
-  void clear();
+  void addPoseVertex(const int16_t &id, const Sophus::SE3d &pose);
+  void addPointVertex(const int16_t &id, const Eigen::Vector3d &point);
+  void addLandmarkEdge(int16_t poseId, int16_t landmarkId, const Eigen::Vector2d &measure);
+  std::vector<Sophus::SE3d> getPose();
   void optimize();
+
+private:
+  void clear();
 
 private:
   std::unique_ptr<g2o::SparseOptimizer> optimizer_;
   g2o::OptimizationAlgorithmLevenberg *solver_;
+  std::vector<g2o::VertexSE3Expmap *> poseVertexs_;
+  std::vector<g2o::VertexPointXYZ *> pointVertexs_;
 };
 
 #endif // __G2O_OPTIMIZATION_H__
--- src/g2o_optimization.cpp
+++ src/g2o_optimization.cpp
@@ -1,13 +1,62 @@
 #include "mono_visual_odometry/g2o_optimization.hpp"
 
-G2O_Optimization::G2O_Optimization() {}
+G2O_Optimization::G2O_Optimization(Eigen::Matrix3d camK) {
+  optimizer_ = std::make_unique<g2o::SparseOptimizer>();
+  solver_ =
+      new g2o::OptimizationAlgorithmLevenberg(g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
+  optimizer_->setAlgorithm(solver_);
+  optimizer_->setVerbose(true);
+}
 
-G2O_Optimization::~G2O_Optimization() {}
+G2O_Optimization::~G2O_Optimization() { delete solver_; }
 
-void G2O_Optimization::updatePoseVertex() {}
+void G2O_Optimization::addPoseVertex(const int16_t &id, const Sophus::SE3d &pose) {
+  g2o::SE3Quat g2oPose(pose.rotationMatrix(), pose.translation());
+  auto vertex = new g2o::VertexSE3Expmap();
+  vertex->setId(id);
+  if (poseVertexs_.size() < 2) {
+    vertex->setFixed(true);
+  }
+  vertex->setEstimate(g2oPose);
+  optimizer_->addVertex(vertex);
+  poseVertexs_.push_back(vertex);
+}
 
-void G2O_Optimization::updateEdge() {}
+void G2O_Optimization::addPointVertex(const int16_t &id, const Eigen::Vector3d &point) {
+  auto vertex = new g2o::VertexPointXYZ();
+  vertex->setId(id);
+  vertex->setMarginalized(true);
+  vertex->setEstimate(point);
+  optimizer_->addVertex(vertex);
+  pointVertexs_.push_back(vertex);
+}
 
-void G2O_Optimization::clear() {}
+void G2O_Optimization::addLandmarkEdge(int16_t poseId, int16_t landmarkId, const Eigen::Vector2d &measure) {
+  auto edge = new g2o::EdgeProjectXYZ2UV();
+  auto poseVertex = dynamic_cast<g2o::OptimizableGraph::Vertex *>(optimizer_->vertex(poseId));
+  auto landmarkVertex = dynamic_cast<g2o::OptimizableGraph::Vertex *>(optimizer_->vertex(landmarkId));
+  g2o::RobustKernelHuber *rk = new g2o::RobustKernelHuber;
 
-void G2O_Optimization::optimize() {}
+  edge->setVertex(0, poseVertex);
+  edge->setVertex(1, landmarkVertex);
+  edge->setMeasurement(measure);
+  edge->setInformation(Eigen::Matrix2d::Identity());
+  edge->setRobustKernel(rk);
+  edge->setParameterId(0, 0);
+  optimizer_->addEdge(edge);
+}
+
+std::vector<Sophus::SE3d> G2O_Optimization::getPose() {
+  std::vector<Sophus::SE3d> poses;
+  return poses;
+}
+
+void G2O_Optimization::clear() {
+  optimizer_->clear();
+  poseVertexs_.clear();
+}
+
+void G2O_Optimization::optimize() {
+  optimizer_->initializeOptimization();
+  optimizer_->optimize(50);
+}
